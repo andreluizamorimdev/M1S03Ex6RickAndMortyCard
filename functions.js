9fea55af79254f0183959146d5e00911ddba2d5c
andreluizamorimdev <andreluizamorim.dev@gmail.com>
2023-02-21 01:13:31 -0300

    Added card styling to api character info

--- functions.js
+++ functions.js
@@ -1,6 +1,25 @@
+let imageCard = document.getElementById('imageCard');
+let nameCard = document.getElementById('nameCard');
+let statusCard = document.getElementById('statusCard');
+let speciesCard = document.getElementById('speciesCard');
+let typeCard = document.getElementById('typeCard');
+let genderCard = document.getElementById('genderCard');
+
 async function getRickAndMortyCharacter() {
     const response = await fetch('https://rickandmortyapi.com/api/character/468');
     const data = await response.json();
+    
+    imageCard.src = data?.image;
+    nameCard.innerHTML = data?.name;
+    statusCard.innerHTML = `status: ${data?.status}`;
+    speciesCard.innerHTML = `espécie:   ${data?.species}`;
+    if(data?.type == '') {
+        typeCard.innerHTML = `tipo: Unknown`;
+    } else { 
+        typeCard.innerHTML = `tipo: ${data?.type}`;
+    }
+    genderCard.innerHTML = `genêro: ${data?.gender}`;
+
     console.log(data);
 }
 
